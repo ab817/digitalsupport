453e23fac7a0893184bfdc5025d9addfa9f2e2f9
Ankit Bhattarai <ablive817@gmail.com>
2024-07-24 13:35:59 +0545

    automate issue number and admin

--- digitallogs/models.py
+++ digitallogs/models.py
@@ -44,6 +44,9 @@ class Serveraccess(models.Model):
     def __str__(self):
         return f"{self.sn} - {self.server_name}"
 
+from django.db import models
+from django.utils.timezone import now
+
 class TechnicalSupportLog(models.Model):
     STATUS_CHOICES = [
         ('Requested', 'Requested'),
@@ -78,16 +81,38 @@ class TechnicalSupportLog(models.Model):
         ('Others', 'Others'),
     ]
 
+    HANDLING_CHOICES = [
+        ('Ankit Bhattarai', 'Ankit Bhattarai'),
+        ('Aakriti Silwal', 'Aakriti Silwal'),
+        ('Bom Bahadur BK', 'Bom Bahadur BK'),
+        ('Bikash Devkota', 'Bikash Devkota'),
+    ]
+
     sn = models.AutoField(primary_key=True)
-    issue_no = models.CharField(max_length=20)
+    issue_no = models.CharField(max_length=20, unique=True, editable=False)
     date = models.DateField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     requested_by = models.CharField(max_length=20, choices=REQUESTED_BY_CHOICES)
     issue_logged_to = models.CharField(max_length=20, choices=ISSUE_LOGGED_TO_CHOICES)
     issue_description = models.TextField()
     reference_code = models.CharField(max_length=50)
-    handling_person = models.CharField(max_length=100)
+    handling_person = models.CharField(max_length=30, choices=HANDLING_CHOICES)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.issue_no
+
+    def save(self, *args, **kwargs):
+        if not self.issue_no:
+            # Generate issue_no
+            last_issue = TechnicalSupportLog.objects.order_by('-sn').first()
+            if last_issue:
+                try:
+                    last_number = int(last_issue.issue_no.split('DBD - ')[-1])
+                except ValueError:
+                    last_number = 0
+                new_number = last_number + 1
+            else:
+                new_number = 1
+            self.issue_no = f'DBD - {new_number:02d}'
+        super().save(*args, **kwargs)
