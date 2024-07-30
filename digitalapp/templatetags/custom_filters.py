7b47f268153553e73f4ae4155142d7237a4a9904
Ankit Bhattarai <ablive817@gmail.com>
2024-07-30 13:01:22 +0545

    truncating FAQ

--- digitalapp/templatetags/custom_filters.py
+++ digitalapp/templatetags/custom_filters.py
@@ -1,9 +1,13 @@
+import re
 from django import template
+from django.utils.html import strip_tags
 
 register = template.Library()
 
 @register.filter
 def truncate_chars(value, max_length):
+    # Strip HTML tags
+    value = strip_tags(value)
     if len(value) > max_length:
         return value[:max_length] + '...'
     return value
